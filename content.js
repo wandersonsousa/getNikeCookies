576ad3532a69fead0bc32ca7d8d58d2de0c73f25
wandersonsousa <wandersonsousa3004@gmail.com>
2020-05-30 02:06:48 -0300

    now get adress id

--- content.js
+++ content.js
@@ -3,3 +3,7 @@ console.log("%c Seu Nike Cookie Será mostrado abaixo","text-shadow: 0 0 10px #f
 console.log(document.cookie)
 
 console.log("%c Copie e cole o texto acima no programa.","text-shadow: 0 0 10px #fff,0 0 20px #fff,0 0 30px #fff,0 0 40px #ff00de,0 0 70px #ff00de,0 0 80px #ff00de,0 0 100px #ff00de,0 0 150px #ff00de;padding:20px;font-size:30px;color:#fff;background:cyan")
+
+console.log("%c O ID de Seu Endereço Será mostrado abaixo","text-shadow: 0 0 10px #fff,0 0 20px #fff,0 0 30px #fff,0 0 40px #ff00de,0 0 70px #ff00de,0 0 80px #ff00de,0 0 100px #ff00de,0 0 150px #ff00de;padding:20px;font-size:30px;color:#fff;background:cyan")
+console.log( document.querySelector("#user-shipping-address-id").value )
+console.log("%c Copie e cole o texto acima no programa.","text-shadow: 0 0 10px #fff,0 0 20px #fff,0 0 30px #fff,0 0 40px #ff00de,0 0 70px #ff00de,0 0 80px #ff00de,0 0 100px #ff00de,0 0 150px #ff00de;padding:20px;font-size:30px;color:#fff;background:cyan")
